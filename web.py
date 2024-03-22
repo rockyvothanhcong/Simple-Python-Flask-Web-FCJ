a093dae3459e47d6cdcfd43b78a561f06840f5ed
rockyvothanhcong <154226619+rockyvothanhcong@users.noreply.github.com>
2024-03-23 02:58:15 +0700

    Create web.py

---
+++ web.py
@@ -0,0 +1,23 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from random import randint
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+myapp = Flask(__name__)
+
+@myapp.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
+@myapp.route("/")
+def hello():
+    return render_template(
+        'test.html', **locals())
+
+if __name__ == "__main__":
+    myapp.run(host='0.0.0.0', port=80)
